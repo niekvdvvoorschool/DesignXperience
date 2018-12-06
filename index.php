a60d9b6c17ceb951ab05c0b1f89cd4cc1d132792
Niek VDV <74327@rocteraa-student.nl>
2018-12-06 11:54:08 +0100

    Leeg project

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="theme-color" content=""/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <link rel="dns-prefetch" href="https://use.fontawesome.com">
+    <link rel="icon" href=""/>
+    <link rel="stylesheet" type="text/css" href="/plugins/resetcss/css/reset.css"/>
+    <link rel="stylesheet" type="text/css" href="/plugins/bootstrap_4.0.0/css/bootstrap.min.css"/>
+    <link rel="stylesheet" type="text/css" href="/dist/css/main.css"/>
+    <title></title>
+    <meta name="keywords" content=""/>
+    <meta name="description" content=""/>
+</head>
+<body>
+
+
+
+<script src="/plugins/jquery/js/jquery.min.js"></script>
+<script src="/plugins/bootstrap_4.0.0/js/popper.min.js"></script>
+<script src="/plugins/bootstrap_4.0.0/js/bootstrap.min.js"></script>
+
+</body>
+</html>
